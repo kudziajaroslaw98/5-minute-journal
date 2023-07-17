f0d9a607c1ea0dbdae1b7f012118da3db85ea432
Jaroslaw Kudzia <jaroslawkudzia73@gmail.com>
2023-07-17 06:37:32 +0200

    added PWA

--- next.config.js
+++ next.config.js
@@ -1,4 +1,27 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
 
-module.exports = nextConfig
+const withPWA = require("next-pwa")({
+    dest: "public",
+    register: true,
+    skipWaiting: true,
+    disable: process.env.NODE_ENV === "development",
+});
+
+module.exports = withPWA({
+    reactStrictMode: true,
+    images: {
+        domains: ['cdn.sanity.io', 'images.unsplash.com'],
+    },
+    compiler: {
+        // ssr and displayName are configured by default
+        styledComponents: true,
+    },
+    webpack(config) {
+        config.module.rules.push({
+            test: /\.svg$/,
+            use: ['@svgr/webpack'],
+        });
+
+        return config;
+    },
+});
